082b164954250bf2b213c3d0b74ccf07437bb8f7
shliew97 <sh.liew97@gmail.com>
2025-04-22 23:58:53 +0000

    fix: allow sending image and video

--- frontend/src/components/Activities/WhatsAppBox.vue
+++ frontend/src/components/Activities/WhatsAppBox.vue
@@ -20,7 +20,7 @@
   </div>
   <div class="flex items-end gap-2 px-3 py-2.5 sm:px-10" v-bind="$attrs">
     <div class="flex h-8 items-center gap-2">
-      <FileUploader @success="(file) => uploadFile(file)" v-if="props.doc.data.conversation_status == 'Accepted'">
+      <FileUploader @success="(file) => uploadFile(file)" v-if="hasAcceptedStatus()">
         <template v-slot="{ openFileSelector }">
           <div class="flex items-center space-x-2">
             <Dropdown :options="uploadOptions(openFileSelector)">
@@ -33,7 +33,7 @@
         </template>
       </FileUploader>
       <IconPicker
-        v-if="props.doc.data.conversation_status == 'Accepted'"
+        v-if="hasAcceptedStatus()"
         v-model="emoji"
         v-slot="{ togglePopover }"
         @update:modelValue="
